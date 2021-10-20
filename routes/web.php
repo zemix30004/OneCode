b4d2f7bc6b8d5ea9d4e39063929343780d72e0e9
zemix30004 <eugenezm@gmail.com>
2021-10-20 17:00:24 +0300

    Added Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use Illuminate\Database\Connectors\PostgresConnector;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::redirect('/iii', '/');
+
+Route::get('/test', TestController::class);
+
+Route::resource('user', UserController::class);
+
+Route::get('posts', [PostController::class, 'index'])->name('posts');
+Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
+Route::post('posts/{post}/comment', [PostController::class, 'comment'])->name('posts.comment');
+
+// Route::post('posts/{post}/likes', [PostController::class, 'delete'])->name('posts.delete');
+// Route::put('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+
+
+
+
+
+Route::fallback(function () {
+    return 'COMEBACK';
 });
