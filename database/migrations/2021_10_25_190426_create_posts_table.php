cb8d8bc4e6599b65e6c0124149ff95d3f5f379fa
zemix30004 <eugenezm@gmail.com>
2021-10-25 22:54:30 +0300

    added migration users posts

---
+++ database/migrations/2021_10_25_190426_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->from(1001);
+            $table->timestamps();
+
+            // $table->bigInteger('user_id')->unsigned();
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+
+            $table->foreignId('user_id')->constrained();
+
+
+            $table->string('title', 1000);
+            $table->text('content');
+
+            $table->boolean('published')->default(true);
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
