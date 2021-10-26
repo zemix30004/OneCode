b6e2129cd3463fe857bef69c4a24765d40af54a0
zemix30004 <eugenezm@gmail.com>
2021-10-26 23:17:47 +0300

    made model Post Currency

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'user_id',
+        'title',
+        'content',
+        'published',
+        'published_at',
+    ];
+
+    protected $casts = [
+
+        'published' => 'bollean',
+    ];
+    protected $dates = [
+
+        'published',
+    ];
+}
